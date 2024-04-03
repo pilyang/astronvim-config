ec015cccfb82df173e431f7cebdbf38c2ee15453
pilyang <pilyang.dev@gmail.com>
2024-04-03 15:08:22 +0900

    plubin: glow - markdown preview with floating

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,14 @@
+return {
+  -- markdown floating preview
+  {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow",
+    opts = {
+      border = "single",
+      width = 150,
+      width_ratio = 0.8,
+      height_ratio = 0.8,
+    },
+  },
+}
