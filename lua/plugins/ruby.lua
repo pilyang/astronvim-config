84dce15d6a66ff075bbbbcac5d3c2495561c1147
pilyang <pilyang.dev@gmail.com>
2024-07-30 17:56:20 +0900

    plugin: setup ruby env

---
+++ lua/plugins/ruby.lua
@@ -0,0 +1,64 @@
+-- Setup for Ruby language
+-- NOTE: base from communicty.pack.ruby
+-- Seperate from community to append addtional plugins and settings for ruby in one file
+--
+-- TODO:
+-- 1. setup for test tool
+-- 2. make sure to if the dap is working well
+-- 3. add ror.nvim (ruby on rails plugin for nvim)
+
+return {
+  -- add treesitter for ruby
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby" })
+      end
+    end,
+  },
+
+  -- add lsp, linter and formatter for ruby
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+    end,
+  },
+  -- formatter setup
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ruby = { "standardrb" },
+      },
+    },
+  },
+
+  -- add debugger for ruby
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = { "suketa/nvim-dap-ruby", config = true },
+  },
+}
