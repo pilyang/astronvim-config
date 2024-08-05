318e121fbd42e9e6d2003913556658548e47d3b5
pilyang <pilyang.dev@gmail.com>
2024-08-05 17:33:39 +0900

    config: change lsp for ruby
    - use ruby-lsp, rubocop
    - disable rubocop from ruby-lsp for duplicated diag

--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -1,13 +1,35 @@
 -- Setup for Ruby language
--- NOTE: base from communicty.pack.ruby
--- Seperate from community to append addtional plugins and settings for ruby in one file
 --
 -- TODO:
 -- 1. setup for test tool
 -- 2. make sure to if the dap is working well
--- 3. add ror.nvim (ruby on rails plugin for nvim)
+-- 3. setup lsp for rails -> done by using ruby-lsp -> continuous check required
 
 return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable-next-line: missing-fields
+      config = {
+        ruby_lsp = {
+          filetype = { "ruby", "eruby" },
+          settings = {
+            ruby = {
+              -- disable default formatter and linter from rubocop which is default for ruby-lsp
+              format = {
+                enable = false,
+              },
+              lint = {
+                enable = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 
   -- add treesitter for ruby
   {
@@ -25,24 +47,37 @@ return {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        -- -- use solargraph
+        -- { "solargraph", "standardrb", "erb-formatter", "erb-lint" }
+        -- use ruby-lsp
+        -- ruby-lsp automatically include ruby lsp rails from v0.3.0 (https://github.com/Shopify/ruby-lsp-rails?tab=readme-ov-file)
+        -- ruby-lsp use rubocop for linting and formatting, but can use standardrb as addon
+        { "ruby_lsp", "rubocop", "erb-formatter", "erb-lint" }
+      )
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        -- { "solargraph", "standardrb" }
+        { "ruby_lsp", "rubocop" }
+      )
     end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "solargraph", "standardrb" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        -- { "solargraph", "standardrb", "erb-formatter", "erb-lint" }
+        { "ruby_lsp", "rubocop", "erb-formatter", "erb-lint" }
+      )
     end,
   },
   -- formatter setup
@@ -51,7 +86,8 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        ruby = { "standardrb" },
+        ruby = { "rubocop" },
+        eruby = { "erb_format" },
       },
     },
   },
@@ -64,6 +100,7 @@ return {
   },
 
   -- ruby on rails setup
+  -- ror.nvim
   {
     -- add mapping hints
     "AstroNvim/astrocore",
@@ -90,5 +127,10 @@ return {
         desc = "󰫏 RoR commands",
       },
     },
+  },
+  -- vim-rails
+  {
+    "tpope/vim-rails",
+    event = "VeryLazy",
   },
 }
